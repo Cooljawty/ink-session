4491d9d90140195d364b1add153ea925672ed7c9
Jacari Harper <jacariguyman2@gmail.com>
2024-05-19 15:09:20 -0700

    Implimented requesting log update from client

--- ink-server.js
+++ ink-server.js
@@ -1,13 +1,29 @@
+//Ink Story
+var Story = require('inkjs').Story;
 
+var fs = require('fs');
+const story_path = "test.ink.json";
+var inkFile = fs.readFileSync(story_path, 'UTF-8').replace(/^\uFEFF/, '');
+var story = new Story(inkFile);
+
+//Server
 const { createServer } = require('http');
 
 const hostname = '127.0.0.1';
 const port = 8080;
 
 const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+	switch (req.url) {
+		case '/update/log':
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'text/plain');
+			res.end(story.Continue());
+			break;
+		default:
+			res.statusCode = 404;
+			res.setHeader('Content-Type', 'text/plain');
+			res.end(`Invalid path: ${req.url}`);
+	}
 });
 
 server.listen(port, hostname, () => {
